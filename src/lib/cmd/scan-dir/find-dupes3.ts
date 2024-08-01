411983fca5d3a6ada251e17d513613d0e46113e9
eaglelizard <whitedtd@gmail.com>
2024-08-01 14:20:53 -0600

    move getPossibleDupes() to own module for findDupes3

--- src/lib/cmd/scan-dir/find-dupes3.ts
+++ src/lib/cmd/scan-dir/find-dupes3.ts
@@ -1,12 +1,11 @@
 
 import assert from 'assert';
 import path from 'path';
-import { Dirent, Stats, WriteStream, createWriteStream } from 'fs';
-import { lstat, readdir, rm } from 'fs/promises';
+import { Dirent, WriteStream, createWriteStream } from 'fs';
+import { readdir, rm } from 'fs/promises';
 
 import { ScanDirOpts } from '../parse-sysmon-args';
-import { logger } from '../../logger';
-import { isObject, isString } from '../../util/validate-primitives';
+import { isObject } from '../../util/validate-primitives';
 import { LineReader, LineReader2, checkDir, getLineReader, mkdirIfNotExist, getLineReader2 } from '../../util/files';
 import { SCANDIR_FIND_DUPES_TMP_DIR, SCANDIR_OUT_DATA_DIR_PATH } from '../../../constants';
 import { Deferred } from '../../../test/deferred';
@@ -16,6 +15,7 @@ import { CliColors, ColorFormatter } from '../../service/cli-colors';
 import { getIntuitiveTimeString } from '../../util/format-util';
 import { GetFileHashesRes, HASH_HWM, MAX_RUNNING_HASHES, getFileHashes } from './find-dupes3/get-file-hashes';
 import { _closeWs } from './find-dupes3/close-ws';
+import { GetPossibleDupesRes, getPossibleDupes } from './find-dupes3/get-possible-dupes';
 
 const SORT_CHUNK_FILE_LINE_COUNT = 100;
 // const SORT_CHUNK_FILE_LINE_COUNT = 250;
@@ -576,142 +576,6 @@ async function getFileDupes(hashFilePath: string, hashCountMap: Map<string, numb
       });
     }
   }
-}
-
-type GetPossibleDupesRes = {
-  possibleDupeSizeMap: Map<number, number>;
-  sizeFilePath: string;
-};
-
-async function getPossibleDupes(filesDataFilePath: string, opts: {
-  nowDate: Date,
-}): Promise<GetPossibleDupesRes> {
-  let possibleDupeMap: Map<number, number>;
-  let getFileSizeRes: GetFileSizeRes;
-  let sizeMap: GetFileSizeRes['sizeMap'];
-  let sizeFilePath: string;
-  let sizeMapIter: IterableIterator<number>;
-  let sizeMapIterRes: IteratorResult<number>;
-
-  getFileSizeRes = await getFileSizes(filesDataFilePath, {
-    nowDate: opts.nowDate,
-  });
-  sizeMap = getFileSizeRes.sizeMap;
-  sizeFilePath = getFileSizeRes.sizeFilePath;
-
-  possibleDupeMap = new Map();
-  sizeMapIter = sizeMap.keys();
-  while(!(sizeMapIterRes = sizeMapIter.next()).done) {
-    let size: number;
-    let fileCount: number | undefined;
-    size = sizeMapIterRes.value;
-    fileCount = sizeMap.get(size);
-    assert(fileCount !== undefined);
-    if(
-      (size > 0)
-      && (fileCount > 1)
-    ) {
-      possibleDupeMap.set(size, fileCount);
-    }
-    sizeMap.delete(size);
-  }
-  sizeMap.clear();
-
-  return {
-    possibleDupeSizeMap: possibleDupeMap,
-    sizeFilePath,
-  };
-}
-
-type GetFileSizeRes = {
-  sizeFilePath: string;
-  sizeMap: Map<number, number>;
-};
-
-async function getFileSizes(filesDataFilePath: string, opts: {
-  nowDate: Date,
-}): Promise<GetFileSizeRes> {
-  let sizeMap: Map<number, number>;
-  let sizeFileName: string;
-  let sizeFilePath: string;
-  let sizeWs: WriteStream;
-  let lineReader: LineReader2;
-  let line: string | undefined;
-  let drainDeferred: Deferred | undefined;
-
-  let rflTimer: Timer;
-
-  // sizeFileName = `${getDateFileStr(opts.nowDate)}_sizes.txt`;
-  sizeFileName = '0_sizes.txt';
-  sizeFilePath = [
-    SCANDIR_OUT_DATA_DIR_PATH,
-    sizeFileName,
-  ].join(path.sep);
-
-  sizeMap = new Map();
-
-  sizeWs = createWriteStream(sizeFilePath);
-  lineReader = await getLineReader2(filesDataFilePath);
-
-  rflTimer = Timer.start();
-
-  while((line = await lineReader.read()) !== undefined) {
-    let fileStats: Stats | undefined;
-    let fileSize: number;
-    let fileSizeCount: number | undefined;
-    let wsRes: boolean;
-    try {
-      fileStats = await lstat(line);
-    } catch(e) {
-      if(
-        isObject(e)
-        && isString(e.code)
-        && (
-          (e.code === 'ENOENT')
-          || (e.code === 'EACCES')
-        )
-      ) {
-        logger.error(`findDuplicates lstat: ${e.code} ${line}`);
-        continue;
-      }
-      console.error(e);
-      throw e;
-    }
-    fileSize = fileStats.size;
-    if((fileSizeCount = sizeMap.get(fileSize)) === undefined) {
-      fileSizeCount = 0;
-    }
-    sizeMap.set(fileSize, fileSizeCount + 1);
-    if(drainDeferred !== undefined) {
-      await drainDeferred.promise;
-    }
-    wsRes = sizeWs.write(`${fileSize} ${line}\n`);
-    if(
-      !wsRes
-      && (drainDeferred === undefined)
-    ) {
-      drainDeferred = Deferred.init();
-      sizeWs.once('drain', () => {
-        assert(drainDeferred !== undefined);
-        drainDeferred.resolve();
-      });
-      drainDeferred.promise.finally(() => {
-        drainDeferred = undefined;
-      });
-    }
-    if(rflTimer.currentMs() > RFL_MOD) {
-      process.stdout.write('.');
-      rflTimer.reset();
-    }
-  }
-  await _closeWs(sizeWs);
-  await lineReader.close();
-  process.stdout.write('\n');
-
-  return {
-    sizeFilePath,
-    sizeMap,
-  };
 }
 
 function getPossibleDupeCount(possibleDupeMap: Map<number, number>): number {
